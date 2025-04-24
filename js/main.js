b3ce142d42f3747cfa8b5068b979db9e11738446
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-04-24 16:00:15 +0200

    add api images to card

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const apiUri = `https://lanciweb.github.io/demo/api/pictures/`;
 const imageGroupEl = document.getElementById("image-group");
-
+const colGeneratorEl = document.getElementById("col-generator");
 axios.get(apiUri).then((response) => {
-  let imageArray = response.data;
+  const imageArray = response.data;
   console.log(imageArray[0].url);
   for (const image of imageArray) {
-    imageGroupEl.innerHTML += `<img src="${image.url}" alt=""/>`;
+    colGeneratorEl.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 20rem">
+            <img class="pin-image" src="./img/pin.svg" alt="">
+              <img src="${image.url}" class="card-img-top p-3" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>`;
   }
 });
