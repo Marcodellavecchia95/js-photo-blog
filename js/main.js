cf31afbbd83fea98a2ada74efc1a18e4d37070c1
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-04 16:25:11 +0200

    add comments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
+// DICHIARAZIONE VARIABILI
 const apiUri = `https://lanciweb.github.io/demo/api/pictures/`;
 const imageGroupEl = document.getElementById("image-group");
 const colGeneratorEl = document.getElementById("col-generator");
-
 const overlayEl = document.querySelector(".overlay");
 
+// CHIAMATA AJAX ALL'API
 axios.get(apiUri).then((response) => {
   const imageArray = response.data;
   console.log(imageArray[0].url);
+
+  // ITERO L'ARRAY DI OGGETTI USANDO UN FOR OF, GENERANDO UNA CARD PER OGNI OGETTO
   for (const image of imageArray) {
     colGeneratorEl.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4">
             <div class="card" style="width: 20rem">
@@ -21,7 +24,11 @@ axios.get(apiUri).then((response) => {
             </div>
           </div>`;
   }
+
+  // DICHIARO IN UNA VARIABILE LE CARTE GENERATE
   const generatedCards = document.querySelectorAll(".card");
+
+  // GENERO PER OGNI CLICK L'HTML NECESSARIO A CREARE UN'ANTEPRIMA CON UN BOTTONE PER TORNARE INDIETRO
   generatedCards.forEach((card) => {
     card.addEventListener("click", () => {
       const cardImage = card.querySelector(".card-img-top");
